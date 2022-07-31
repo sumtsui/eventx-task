299a002076931519c92e8f89f3c52ca93e06b813
Sum Tsui <sumtsui@gmail.com>
2022-07-31 09:30:10 +0800

    dev: PriceBox style

--- client/src/components/PriceBox.tsx
+++ client/src/components/PriceBox.tsx
@@ -4,18 +4,21 @@ import "./PriceBox.css";
 
 function PriceBox({ data }: { data: TradingInfo }) {
   const { name, priceUsd, volumeUsd24Hr, changePercent24Hr } = data;
+
+  const change = Number(changePercent24Hr) > 0 ? "up" : "down";
+
   return (
     <div className="price-box">
-      <h2>{name}</h2>
-      <h3>{priceUsd}</h3>
+      <h2 className="name">{name}</h2>
+      <h3 className="price">{priceUsd}</h3>
       <div className="details">
         <div className="volume">
           <div>volume:</div>
           <div>{volumeUsd24Hr}</div>
         </div>
-        <div className="change">
+        <div className={"change " + change}>
           <div>change:</div>
-          <div>{changePercent24Hr}</div>
+          <div className="value">{changePercent24Hr}</div>
         </div>
       </div>
     </div>
